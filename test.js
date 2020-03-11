2be2b29a41db8fb799b5f7d91c4899326d709cd7
Neo Kim <logicfeel@nate.com>
2020-03-11 09:31:01 +0900

    테스트

---
+++ test.js
@@ -0,0 +1,28 @@
+
+
+
+/**
+ * 이건 테스트요
+ */
+function ABC(abc) {
+
+}
+
+
+
+ABC()
+
+
+
+var a;
+
+
+a = "ABCDEDF";
+
+a = a.substr(1,3);
+
+console.log(WebAssembly.Module.name);
+
+
+console.log("------------------------");
+console.log(a);
